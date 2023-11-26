6a007aade2bce926b8d24730dab14b32f6d723f8
Gilles de PERETTI <gillesdeperetti.pro@gmail.com>
2023-11-26 15:46:26 -0300

    introduction

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ data = load_data()
 # PAGES
 if choose == "Introduction":
     st.title('Deep Dive in Airbnb Data')
+    st.write(introduction)
+    wordcloud_fig = plot_wordcloud(data)
+    st.pyplot(wordcloud_fig)
 
 if choose == "Data Overview": 
     st.title('Extraction Transformation Load')
---
+++ src/texts.py
@@ -0,0 +1,11 @@
+introduction = """
+        In this project, I undertake the analysis of Airbnb listings across several major European cities. The data, obtained from [Inside Airbnb](http://insideairbnb.com/get-the-data/), circumvents the challenges of accessing the Airbnb API directly and provides rich datasets for comprehensive analysis.
+
+        The goal is to design and execute a complete data processing workflow. This includes the ETL (Extract, Transform, Load) operations for data curation, the development of dynamic visualizations using **Plotly**, and the construction of machine learning models aimed at predicting listing prices based on various attributes.
+
+        I started with datasets from eight iconic cities: **Florence, Amsterdam, Brussels, Barcelona, Lisbon, Berlin, Paris, and London**. These datasets are well-suited for thorough data analysis and visualization tasks.
+
+        The consolidation of these datasets resulted in a combined dataset with **17 features encompassing a total of 237,238 Airbnb listings**. Among these, the "name" feature, although seemingly unprocessed, revealed a treasure trove of insights.
+
+        ## Exploring the `Name` Variable: A Glimpse Into Data Diversity
+        """
