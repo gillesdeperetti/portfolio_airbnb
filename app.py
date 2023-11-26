3135888aad270b9bfdfc8bea14f50e966858c904
Gilles de PERETTI <gillesdeperetti.pro@gmail.com>
2023-11-26 15:21:18 -0300

    Creating the structure for Streamlit

---
+++ app.py
@@ -0,0 +1,36 @@
+# LIBRARIES
+from src.utils import *
+from src.data_loader import *
+from src.visualizations import *
+from src.texts import *
+from src.config import page_config
+from static.styles import *
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(**page_config)
+load_css('static/styles/custom.css')
+choose = setup_sidebar()
+data = load_data()
+
+# PAGES
+if choose == "Introduction":
+    st.title('Deep Dive in Airbnb Data')
+
+if choose == "Data Overview": 
+    st.title('Extraction Transformation Load')
+
+if choose == "Data Visualization":
+    st.title('Exploratory Data Analysis')
+
+if choose == "Data to Map":
+    st.title('Filter, Find, and Visualize')
+
+if choose == "Modeling":
+    st.title('Predictive modeling steps')
+
+if choose == "Try it yourself":
+    st.title('Model user interface')
+
+if choose == "Conclusion":
+    st.title('Conclusion')
---
+++ src/config.py
@@ -0,0 +1,12 @@
+# Custom colors 
+rausch = '#FF5A5F'
+babu = '#00A699'
+arches = '#FC642D'
+foggy = '#767676'
+
+# Streamlit page config
+page_config = {
+    "page_title": "AirBnB Data Dive",
+    "page_icon": "🔍",
+    "layout": "wide"
+}
